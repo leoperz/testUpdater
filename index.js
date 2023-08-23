6222a609134bbeeb7d420dd63c211bb60665a418
Perez <ljperez@bpba.com.ar>
2023-08-22 21:50:02 -0300

    cambios

--- index.js
+++ index.js
@@ -1,56 +1,55 @@
-const { app, BrowserWindow } = require('electron');
-const {autoUpdater} = require('electron-updater');
-const path = require('path');
-const log = require('electron-log');
-log.transports.file.resolvePath = ()=> path.join(__dirname, 'logs/main.log');
-log.log("application version", + app.getVersion());
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
-let win  
- function createWindow(){
-    win = new BrowserWindow({width:300, height:400})
-    win.loadFile('./index.html')
- }
+let mainWindow;
 
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 
- app.on('ready',()=>{
+  mainWindow.once('ready-to-show', () => {
+   autoUpdater.checkForUpdatesAndNotify();
+ });
+
+}
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
     createWindow();
-    autoUpdater.checkForUpdatesAndNotify()
- })
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+   mainWindow.webContents.send('update_available');
+ });
+ autoUpdater.on('update-downloaded', () => {
+   mainWindow.webContents.send('update_downloaded');
+ });
 
 
- autoUpdater.on("update-available",()=>{
-    log.info("update-available")
- })
-
- 
- autoUpdater.on("update-not-available",()=>{
-   log.info("update-not-available")
-})
-
-
-autoUpdater.on('error',()=>{
-   log.info('error');
-})
-
-autoUpdater.on('download-progress',(progress)=>{
-   log.info('download-progress');
-   log.info(progress);
-})
-
- autoUpdater.on('checking-for-update',()=>{
-    log.info('checking-for-update');
- })
-
-
- autoUpdater.on('download-progress',()=>{
-    log.info('download-progress');
- })
-
-
-
-
-
- autoUpdater.on('update-downloaded',()=>{
-    log.info('update-downloaded');
- })
-
+ ipcMain.on('restart_app', () => {
+   autoUpdater.quitAndInstall();
+ });
